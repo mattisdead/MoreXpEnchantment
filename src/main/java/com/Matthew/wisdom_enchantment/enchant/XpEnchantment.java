1cfc86d42208334c1c56dcafd5db3ff0361a4ee7
Matt <deadismatt@gmail.com>
2023-08-09 12:50:54 +0200

    Wisdom is no longer compatible with mending.

--- src/main/java/com/Matthew/wisdom_enchantment/enchant/XpEnchantment.java
+++ src/main/java/com/Matthew/wisdom_enchantment/enchant/XpEnchantment.java
@@ -3,6 +3,8 @@ package com.Matthew.wisdom_enchantment.enchant;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.Enchantments;
+import org.jetbrains.annotations.NotNull;
 
 public class XpEnchantment extends Enchantment {
     public XpEnchantment() {
@@ -11,5 +13,9 @@ public class XpEnchantment extends Enchantment {
     @Override
     public int getMaxLevel() {
         return 3;
+    }
+    @Override
+    protected boolean checkCompatibility(@NotNull Enchantment other) {
+        return super.checkCompatibility(other) && other != Enchantments.MENDING;
     }
 }
